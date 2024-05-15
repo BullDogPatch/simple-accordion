ed4d7fe0641e281dab5f20c627936531899d610a
BullDogPatch <mancityvsliverpoolfc@outlook.com>
2024-05-15 20:10:46 +0100

    moved the event handler to the outer div

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -4,10 +4,10 @@ function AccordionItem({ num, title, text }) {
   const [open, setOpen] = useState(false);
   const toggleOpen = () => setOpen((prev) => !prev);
   return (
-    <div className="item">
+    <div className="item"  onClick={toggleOpen}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
-      <p onClick={toggleOpen} className="icon">
+      <p className="icon">
         {open ? '+' : '-'}
       </p>
       {open && <div className="content-box">{text}</div>}
