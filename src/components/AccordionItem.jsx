7150907c47ca424aac345979c2c77549bf430308
BullDogPatch <mancityvsliverpoolfc@outlook.com>
2024-05-15 20:24:08 +0100

    add conditional class

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -4,9 +4,9 @@ function AccordionItem({ num, title, text }) {
   const [open, setOpen] = useState(false);
   const toggleOpen = () => setOpen((prev) => !prev);
   return (
-    <div className="item"  onClick={toggleOpen}>
+    <div className={`item ${open ? 'open' : ''}`}  onClick={toggleOpen}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="text">{title}</p>
+      <p className="title">{title}</p>
       <p className="icon">
         {open ? '+' : '-'}
       </p>
